9d1c023affc3e92335093756e2aff74f4af1f852
Otrifix <otrifix@proton.me>
2025-01-26 23:49:54 +0100

    Added function to get prayer times using longitude and latitude

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import requests
 import json
 
-prev_time_sec = 0
+#prev_time_sec = 0
 
 # Function to get longitude and latitude using IP address
 def get_location():
@@ -16,7 +16,18 @@ def get_location():
         longitude=data["longitude"]
         return (latitude,longitude)
 
+def get_prayer_times(latitude,longitude):
+    current_date = time.localtime()
+    url = f"https://api.aladhan.com/v1/calendar/{current_date[0]}/1?latitude={latitude}&longitude={longitude}&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6"
+    respons = requests.get(url)
+    if respons.status_code == 200:
+        data = respons.json()
+        return (data["data"])
 
+
+latitude = get_location()[0]
+longitude = get_location()[1] 
+print(get_prayer_times(latitude,longitude))
 
 # while True:
 #     local_time = time.localtime()
