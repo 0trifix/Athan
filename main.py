b6224e5b099dcba01a305fd83ed6de02d7f3b2b0
Otrifix <otrifix@proton.me>
2025-02-12 22:56:32 +0100

    added more functions to get the daily prayer times and an untested notification function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
+import subprocess
 import requests
-import json
 
 #prev_time_sec = 0
 
@@ -15,27 +15,41 @@ def get_location():
         latitude=data["latitude"]
         longitude=data["longitude"]
         return (latitude,longitude)
+    return None
 
-def get_prayer_times(latitude,longitude):
+def get_monthly_prayer_times(latitude,longitude):
     current_date = time.localtime()
     url = f"https://api.aladhan.com/v1/calendar/{current_date[0]}/1?latitude={latitude}&longitude={longitude}&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6"
     respons = requests.get(url)
     if respons.status_code == 200:
         data = respons.json()
         return (data["data"]) # Returns the prayer times of the month in list of dictionary's 
+    return None
 
+def get_today_prayer_times():
+    location = get_location()
+    current_date = time.localtime()
+    today_prayer_times = get_monthly_prayer_times(location[0],location[1])[current_date[2]-1]["timings"]
+    return today_prayer_times
 
-latitude = get_location()[0]
-longitude = get_location()[1] 
-month_prayer_times = get_prayer_times(latitude,longitude)
+def show_prayer_times():
+    list_of_prayers = ["Fajr","Sunrise","Dhuhr","Asr","Maghrib","Isha"]
+    today_prayer_times = get_today_prayer_times()
+    for key in today_prayer_times:
+        if key in list_of_prayers:
+            print(f"{key}: {today_prayer_times[key]}")
+    return None
 
-for day in month_prayer_times:
-    print(day["timings"])
-    print("")
+def prayer_notification():
+    list_of_prayers = ["Fajr","Sunrise","Dhuhr","Asr","Maghrib","Isha"]
+    today_prayer_times = get_today_prayer_times()
+    current_time = time.localtime()
+    current_time_sec = current_time[3]*3600 + current_time[4]*60 + current_time[5]
+    for key in today_prayer_times:
+        if key in list_of_prayers:
+            prayer_time = today_prayer_times[key]
+            prayer_time_sec = int(prayer_time.split(":")[0])*3600 + int(prayer_time.split(":")[1])*60
+            if current_time_sec == prayer_time_sec:
+                subprocess.run(["notify-send",f"{key} prayer time"])
+    return None
 
-# while True:
-#     local_time = time.localtime()
-#     if local_time[4] != prev_time_sec:
-#         print(f"Current time: {local_time[3]:02d}:{local_time[4]:02d}")
-#         prev_time_sec = local_time[4]
-#         get_location()
